511cec308fc13adcbbfef55c3732739d6c0570d0
miralsidev <miralsidev@gmail.com>
2024-05-20 18:08:08 +0530

    addocde

--- src/App.js
+++ src/App.js
@@ -1,70 +1,11 @@
 import "./App.css";
-import axios from 'axios';
-import React, { useState } from 'react';
+import Reg from '../src/Reg'
+import DisplayData from './DisplayData'
 function App() {
-
-  const [formData, setFormData] = useState({
-    fname: '',
-    lname: '',
-    email: '',
-  });
-
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    const { name, value } = event.target;
-    setFormData((preValue)=>{
-      console.log("preValue==", preValue);
-      return {
-        ...preValue,
-        [name]: value
-      };
-    })
-  };
-  const OnSubmitReg = async(event)=>{
-    event.preventDefault();
-    try {
-      console.log("from data = ",formData);
-      const response = await axios.post('http://localhost:5000/api/addUser',formData);
-      alert(response.data);
-    } catch (error) {
-      console.error('There was an error submitting the form!', error);
-    }
-  }
   return (
     <>
-      <h1>I am register here = {formData.fname}</h1>
-      <div>
-        <form onSubmit = {OnSubmitReg}>
-        <div>
-          <label>First Name:</label>
-          <input
-            type="text"
-            name="fname"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input
-            type="text"
-            name="lname"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-        </form>
-      </div>
+      <Reg />
+      <DisplayData/>
     </>
   );
 }
---
+++ src/DisplayData.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+function DisplayData() {
+   const [items, setItems] = useState([]);
+   console.log(items, "itemsitemsitemsitems")
+   useEffect(() => {
+      axios.get('http://localhost:5000/api/displayUser')
+         .then(response => {
+            console.log("response.data", response.data);
+            setItems(response.data);
+         })
+         .catch(error => {
+            console.error('Error fetching data:', error);
+         });
+   }, []);
+
+   return (
+      <div>
+         <h1>display data abc</h1>
+         <div className="App">
+            <h1>Items List</h1>
+            <ul>
+               {items.data?.map(item =>
+                  <li key={item._id}>{item.fname}</li>
+               )}
+            </ul>
+         </div>
+      </div>
+   )
+}
+
+export default DisplayData
