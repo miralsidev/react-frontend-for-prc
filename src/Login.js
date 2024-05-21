3abb14a595530af936a3792c0484de5ee4e3f8da
miralsidev <miralsidev@gmail.com>
2024-05-21 22:49:44 +0530

    addcode21

---
+++ src/Login.js
@@ -0,0 +1,69 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+function Login() {
+    
+    const [formData, setFormData] = useState({
+        email: '',
+        password:'',
+      });
+      const [message, setMessage] = useState('');
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    setFormData((preValue) => {
+      console.log("preValue==", preValue);
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+        try {
+            // const response = await axios.post('http://localhost:5000/api/login', { email, password });
+            const response = await axios.post('http://localhost:5000/api/login',formData);
+            localStorage.setItem('token', response.data.token);
+            alert("Login successful")
+            setMessage('Login successful');
+        } catch (error) {
+            // console.error('Error:', error.response.data);
+            // setMessage('Invalid username or password');
+            console.error('Error:', error);
+            // setMessage(error.response.data.message);
+            if (error.response && error.response.data) {
+                alert("Invalid username or password")
+                setMessage('Invalid username or password');
+              } else {
+                alert("Server error. Please try again later.")
+                setMessage('Server error. Please try again later.');
+              }
+        }
+    
+  }
+  return (
+    <>
+      <div>Login</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" name='email' onChange={handleChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+           name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>{message}</p>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/Reg.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+function Reg() {
+
+    const [formData, setFormData] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        password:'',
+      });
+    
+      const handleChange = (event) => {
+        console.log(event.target.value);
+        const { name, value } = event.target;
+        setFormData((preValue)=>{
+          console.log("preValue==", preValue);
+          return {
+            ...preValue,
+            [name]: value
+          };
+        })
+      };
+      const OnSubmitReg = async(event)=>{
+        event.preventDefault();
+        try {
+          console.log("from data = ",formData);
+          const response = await axios.post('http://localhost:5000/api/addUser',formData);
+          alert(response.data);
+        } catch (error) {
+          console.error('There was an error submitting the form!', error);
+        }
+      }
+
+  return (
+    <>
+    <h1>I am register here = {formData.fname}</h1>
+    <div>
+      <form onSubmit = {OnSubmitReg}>
+      <div>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="fname"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="lname"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+      </form>
+    </div>
+    </>
+
+  )
+}
+
+export default Reg
